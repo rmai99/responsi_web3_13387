2ac35974dafebd13dc19b80edba4dea3227649f6
wafiseptya <septya.wafi@gmail.com>
2019-05-23 10:12:11 +0700

    Updates

---
+++ precache-manifest.71f30e2420baceb636411ab5992a29d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e04f3d1c44d3e1c1c85",
+    "url": "/responsi_web3_13387/static/js/runtime~main.1e04f3d1.js"
+  },
+  {
+    "revision": "a48b6c728512058df6b4",
+    "url": "/responsi_web3_13387/static/js/main.a48b6c72.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/responsi_web3_13387/static/js/2.681b5105.chunk.js"
+  },
+  {
+    "revision": "a48b6c728512058df6b4",
+    "url": "/responsi_web3_13387/static/css/main.cc43deb7.chunk.css"
+  },
+  {
+    "revision": "2400317e45776f30cb807a2e4a89cdee",
+    "url": "/responsi_web3_13387/index.html"
+  }
+];
